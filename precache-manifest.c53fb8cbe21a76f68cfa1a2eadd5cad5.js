2ea1028bd0bd384bc485a89d30a5bf200af531f0
나영 <nayoung@nayeong-ui-MacBook-Pro.local>
2018-10-18 17:40:53 +0900

    Updates

---
+++ precache-manifest.c53fb8cbe21a76f68cfa1a2eadd5cad5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "688ddf114a1dde37e88c",
+    "url": "/movie_app/static/js/main.688ddf11.chunk.js"
+  },
+  {
+    "revision": "76fda7dd3b82a3a83c5c",
+    "url": "/movie_app/static/js/1.76fda7dd.chunk.js"
+  },
+  {
+    "revision": "688ddf114a1dde37e88c",
+    "url": "/movie_app/static/css/main.f69f1850.chunk.css"
+  },
+  {
+    "revision": "b70aa703d1e551e81a6c90a8958474b1",
+    "url": "/movie_app/index.html"
+  }
+];
